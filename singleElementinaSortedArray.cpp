b2a6d3acc53bfe1432a30ae54c83aa8bfa6fbcb6
devanshdecode <83800653+devanshdecode@users.noreply.github.com>
2023-06-17 21:53:32 +0530

    Create singleElementinaSortedArray.cpp

---
+++ singleElementinaSortedArray.cpp
@@ -0,0 +1,9 @@
+// Code by - Devansh Lohiya
+    int singleNonDuplicate(vector<int>& nums) {
+        int n=nums.size();
+        int ele=0;
+        for(int i=0;i<n;i++){
+            ele=ele^nums[i];
+        }
+        return ele;
+    }
