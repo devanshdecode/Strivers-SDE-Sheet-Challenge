de6ece0d5daf66f52f51000ce6e09f4df192d6f4
Devansh Lohiya <83800653+devanshdecode@users.noreply.github.com>
2023-07-06 22:29:37 +0530

    Add files via upload

--- countDistinctElementinEveryKSizeWindow.cpp
+++ countDistinctElementinEveryKSizeWindow.cpp
@@ -1,26 +1,28 @@
-// Code by - Devansh Lohiya
+#include <bits/stdc++.h>
 vector<int> countDistinctElements(vector<int> &arr, int k)
 
 {
 
-vector<int> ans;
+    vector<int> ans;
 
-unordered_map<int,int>mp;
+     unordered_map<int,int>mp;
 
-for(int i=0;i<arr.size();i++){
+    for(int i=0;i<arr.size();i++){
 
-mp[arr[i]]++;
+        mp[arr[i]]++;
 
-if(i>=k-1){
+        if(i>=k-1){
 
-ans.push_back(mp.size());
+            ans.push_back(mp.size());
 
-if(--mp[arr[i-k+1]]==0)mp.erase(arr[i-k+1]);//if its freq is 0 remove it from map
+            if(--mp[arr[i-k+1]]==0)
+                mp.erase(arr[i-k+1]);//if its freq is 0 remove it from map
+
+        }
+
+    }
+
+    return ans;
 
 }
-
-}
-
-return ans;
-
-}
+countDistinctElementinEveryKSizeWindow.cpp
