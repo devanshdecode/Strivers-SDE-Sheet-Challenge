a719ceac48e949019887fa51b0c7ff6f71a84e80
Devansh Lohiya <83800653+devanshdecode@users.noreply.github.com>
2023-07-16 23:10:29 +0530

    Create deleteNodeinLinkedlist.cpp

---
+++ deleteNodeinLinkedlist.cpp
@@ -0,0 +1,7 @@
+// Code by - Devansh Lohiya
+void deleteNode(LinkedListNode<int> * node) {
+    // Write your code here.
+    LinkedListNode<int> *temp=node->next;
+    node->data=temp->data;
+    node->next=temp->next;
+}
