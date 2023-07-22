576b8ae19ea250d62023f2ae58c4a7ece40625aa
Devansh Lohiya <83800653+devanshdecode@users.noreply.github.com>
2023-07-22 21:54:37 +0530

    Create flatternlinkedlist.cpp

---
+++ flatternlinkedlist.cpp
@@ -0,0 +1,91 @@
+// Code by - Devansh Lohiya
+Node* merge(Node* head1, Node* head2)
+
+{
+
+    if(head1 == nullptr)
+
+    {
+
+        return head2;
+
+    }
+
+    if(head2 == nullptr)
+
+    {
+
+        return head1;
+
+    }
+
+ 
+
+    Node* ans = nullptr;
+
+ 
+
+    if(head1->data <= head2->data)
+
+    {
+
+        ans = head1;
+
+        ans->child = merge(head1->child , head2);
+
+    }
+
+    else
+
+    {
+
+        ans = head2;
+
+        ans->child = merge(head1,head2->child);
+
+    }
+
+ 
+
+    return ans;
+
+}
+
+ 
+
+Node* flattenLinkedList(Node* head)
+
+{
+
+    if(head==nullptr || head->next == nullptr)
+
+    {
+
+        return head;
+
+    }
+
+ 
+
+          Node *newLL = flattenLinkedList(head->next);
+
+ 
+
+          head->next = nullptr;
+
+ 
+
+          Node *newHead = merge(newLL, head);
+
+        
+
+ 
+
+        return newHead;
+
+}
+
+// T.C =  O(nlogn)
+
+// S.C = O(logn)
+
